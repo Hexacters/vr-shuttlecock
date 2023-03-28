06f085ed89071b04c07da43008dd098d8fa52b68
Venkat <aboutvenkatuser@gmail.com>
2023-03-28 16:23:20 +0530

    Updates

---
+++ Player.jsx
@@ -0,0 +1,31 @@
+
+
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export function Model(props) {
+  const { nodes, materials } = useGLTF('/player.glb')
+  return (
+    <group {...props} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]} scale={1.06}>
+        <group position={[-0.29, -0.26, 1.08]} rotation={[0.24, 0.28, -0.88]} scale={0.09}>
+          <mesh geometry={nodes['qiupai_03_-_Default_0'].geometry} material={materials['03_-_Default']} />
+          <mesh geometry={nodes['qiupai_02_-_Default_0'].geometry} material={materials['02_-_Default']} />
+          <mesh geometry={nodes['qiupai_08_-_Default_0'].geometry} material={materials['08_-_Default']} />
+          <mesh geometry={nodes['qiupai_08_-_Default_0_1'].geometry} material={materials['08_-_Default']} />
+          <mesh geometry={nodes['qiupai_08_-_Default_0_2'].geometry} material={materials['08_-_Default']} />
+        </group>
+        <group position={[0, -0.07, 0]}>
+          <mesh geometry={nodes['Model_Material_#54_0'].geometry} material={materials.Material_54} />
+          <mesh geometry={nodes['Model_Material_#54_0_1'].geometry} material={materials.Material_54} />
+        </group>
+        <group position={[-0.02, -0.73, 0.91]} rotation={[-2.64, -0.52, -2.25]} scale={0.01}>
+          <mesh geometry={nodes.qiu__lambert5_0.geometry} material={materials.lambert5} />
+          <mesh geometry={nodes['qiu_02_-_Default_0'].geometry} material={materials['02_-_Default_0']} />
+        </group>
+      </group>
+    </group>
+  )
+}
+
+useGLTF.preload('/player.glb')
